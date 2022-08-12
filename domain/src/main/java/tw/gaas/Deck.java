93372e9acdb1f5ddd82b5e99e73c7a524a9418ff
iversonfir <iversonfir@gmail.com>
2022-08-12 16:11:49 +0800

    [Refactor] Deck standard108Cards Method
    TDD DeckTest givenUnoGameInit_whenGetStandard108Cards_thenHas19RedNumberCard Method

--- domain/src/main/java/tw/gaas/Deck.java
+++ domain/src/main/java/tw/gaas/Deck.java
@@ -1,23 +1,24 @@
 package tw.gaas;
 
 import java.util.Stack;
+import java.util.stream.Stream;
 
 public class Deck {
-    Stack<Card> cards = new Stack<>();
+    private Stack<Card> drawPile = new Stack<>();
 
     public static Deck standard108Cards() {
         Deck deck = new Deck();
         Number[] numbers = Number.values();
         for (int j = 0; j < 2; j++) {
             for (int i = 1; i < numbers.length; i++) {
-                deck.cards.push(new NumberCard(Color.RED, numbers[i]));
+                deck.drawPile.push(new NumberCard(Color.RED, numbers[i]));
             }
         }
-        deck.cards.push(new NumberCard(Color.RED, Number.ZERO));
+        deck.drawPile.push(new NumberCard(Color.RED, Number.ZERO));
         return deck;
     }
 
-    public Stack<Card> getCards() {
-        return cards;
+    public Stream<Card> stream() {
+        return drawPile.stream();
     }
 }
